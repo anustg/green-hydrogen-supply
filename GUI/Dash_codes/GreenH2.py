4eaec026e125b71ef3878c3c308b0bf5b9229b23
ahmadmojiri <98721000+ahmadmojiri@users.noreply.github.com>
2022-12-02 09:55:59 +1100

    Added Darwin for Tara

--- GUI/Dash_codes/GreenH2.py
+++ GUI/Dash_codes/GreenH2.py
@@ -47,7 +47,8 @@ locations = [{'label':'Burnie 1', 'value':'Burnie 1'},
              {'label':'Upper Spencer Gulf 1','value':'Upper Spencer Gulf 1'},
              {'label':'Upper Spencer Gulf 2','value':'Upper Spencer Gulf 2'},
              {'label':'Upper Spencer Gulf 3','value':'Upper Spencer Gulf 3'},
-             {'label':'Upper Spencer Gulf 4','value':'Upper Spencer Gulf 4'}]
+             {'label':'Upper Spencer Gulf 4','value':'Upper Spencer Gulf 4'},
+             {'label':'Darwin','value':'Darwin'}]
 
 results_var = [{'label':'PV output', 'value':'pv_pout'},
                {'label':'Wind output', 'value':'wind_pout'},
